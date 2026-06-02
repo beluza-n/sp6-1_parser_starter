1ffcef239ee1573b197824bc233989272864529b
beluza <beluza@mail.ru>
2026-06-03 00:33:45 +0300

    parse_meta

--- parser.js
+++ parser.js
@@ -1,8 +1,30 @@
 // @todo: напишите здесь код парсера
+function parse_meta() {
+    const result = {};
+    result.language = document.querySelector('html').lang;
+    result.head = document.querySelector('head title').textContent.split('—')[0].trim();
+    result.keywords = document.querySelector('meta[name="keywords"]')
+        .content
+        .split(',')
+        .map((s) => s.trim());
+    result.description = document.querySelector('meta[name="description"]').content;
+    result.opengraph = {};
+    document.querySelectorAll('meta[property^="og:"]')
+        .forEach((el) => {
+            const key = el.getAttribute('property').replace('og:', '');
+            let value = el.content;
+            if (key === 'title') {
+                value = value.split('—')[0].trim();
+            }
+            result.opengraph[key] = value;
+        })
+    
+    return result;
+}
 
 function parsePage() {
     return {
-        meta: {},
+        meta: parse_meta(),
         product: {},
         suggested: [],
         reviews: []
