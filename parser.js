683996bf6c4dc323426d306fb53b73e63982f298
beluza <beluza@mail.ru>
2026-06-03 01:06:51 +0300

    начало parse_product

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 // @todo: напишите здесь код парсера
 function parse_meta() {
-    const result = {};
+    let result = {};
     result.language = document.querySelector('html').lang;
     result.head = document.querySelector('head title').textContent.split('—')[0].trim();
     result.keywords = document.querySelector('meta[name="keywords"]')
@@ -22,10 +22,27 @@ function parse_meta() {
     return result;
 }
 
+function parse_product() {
+    let result = {};
+    result.id = document.querySelector('body div').id;
+    const images = [... document.querySelectorAll('.product .preview nav img')]
+    const disabled_images = images.filter((img) => img.closest('button').hasAttribute('disabled'));
+    const rest_images = images.filter((img) => !img.closest('button').hasAttribute('disabled'));
+    result.images = [...disabled_images, ...rest_images].map((img) => ({
+        preview: img.scroll,
+        full: img.dataset.src,
+        alt: img.alt,
+    }));
+    result.isLiked = document.querySelector('.like').classList.contains('active');
+    result.name = document.querySelector('h1').textContent;
+    return result;
+
+}
+
 function parsePage() {
     return {
         meta: parse_meta(),
-        product: {},
+        product: parse_product(),
         suggested: [],
         reviews: []
     };
